a327cadc91f37e0167b2ccf062b06d5adc3d3f85
Alan Glennon <glennon@gmail.com>
2018-04-05 01:08:34 -0700

    start creating functions to satisfy tests

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,22 @@
 // All code should be written in this file.
+
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+function setPlayerMoves (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+
+};
+
+function getRoundWinner () {
+
+};
